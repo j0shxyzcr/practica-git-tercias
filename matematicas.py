243dc8dc5e2e8d21becf3770e09d2018168f9bb7
Joshua Cruz <rl570817@gmail.com>
2026-08-04 18:41:27 -0600

    codigo de maximos y minimos

--- matematicas.py
+++ matematicas.py
@@ -1,2 +1,7 @@
 numeros = [12, 45, 7, 23, 64, 10, 89, 3]
 print("Resultados del analisis estadistico:")
+
+# esto lo hizo joshua
+maximo = max(numeros)
+minimo = min(numeros)
+print(f"Maximo: {maximo} | Minimo: {minimo}")
